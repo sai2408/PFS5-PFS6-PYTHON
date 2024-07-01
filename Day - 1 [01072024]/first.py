620c550bb6cbdbea8139313f36d107935c62027c
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-01 16:44:03 +0530

    Add files via upload

---
+++ Day - 1 [01072024]/first.py
@@ -0,0 +1,63 @@
+#Fundamentals
+'''
+#Statement
+print("Hello World")
+#Multiline Comments
+abc
+xyz
+mnpo
+#Kweywords in Python
+print("Abc")
+import keyword
+print(keyword.kwlist)
+a = 10
+print(a)
+x = 1
+y = 2
+print(x)
+print(y)
+#Multiple assignment
+x,y = 1,2
+print(x)
+print(y)
+n1 = 20
+n2 = 10
+print(n1)
+print(n2)
+#Swapping
+n1,n2 = n2,n1
+print(n1)
+print(n2)
+#Deletion of variables
+print(n1)
+del n1
+print(n1)
+'''
+#Variables
+a = 10
+b = 20
+c,d = 30,10
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
