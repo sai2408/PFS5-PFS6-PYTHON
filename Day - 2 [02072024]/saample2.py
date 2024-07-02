bfd0281f21de1319cc49fa2d93b7db29a822c22d
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-02 16:33:58 +0530

    Add files via upload

---
+++ Day - 2 [02072024]/saample2.py
@@ -0,0 +1,160 @@
+#Type conversions
+#Int -> Float,String
+'''
+a = 10
+b = 20
+print(type(a))
+print(type(b))
+c = float(a)
+d = str(b)
+print(c)
+print(type(c))
+print(type(d))
+'''
+#Float -> Int,Strings
+'''
+a = 10.99
+b = 14.765
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+c = int(a)
+print(c)
+print(type(c))
+d = str(b)
+print(d)
+print(type(d))
+'''
+#Strings -> Int,Float,List,Tuple,Dict,set
+'''
+a = "10"
+b = "Sai"
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+c = int(a)
+print(c)
+print(type(c))
+#d = int(b)
+#print(b)
+#print(type(b))
+d = "10.45"
+e = float(d)
+print(e)
+print(type(e))
+x = "12"
+y = "34"
+print(x)
+print(type(x))
+x1 = list(x)
+print(x1)
+print(type(x1))
+x2 = tuple(x)
+print(x2)
+print(type(x2))
+x3 = zip(x,y)
+x3 = dict(x3)
+print(x3)
+print(type(x3))
+'''
+#List -> String,Tuple,Dict
+'''
+a = ["Insta","Facebook"]
+b = ["iamsai2408","Sai vardhan"]
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+c = tuple(a)
+print(c)
+print(type(c))
+d = str(b)
+print(d)
+print(type(d))
+e = zip(a,b)
+e1 = dict(e)
+print(e1)
+print(type(e1))
+'''
+#Tuple -> String,List
+'''
+a = ("a","b","c")
+b = (1,2,3)
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+c = str(a)
+print(c)
+print(type(c))
+d = list(b)
+print(d)
+print(type(d))
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
