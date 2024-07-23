61a3ca0d0b2bf2436e6dc82cb67315ac9d7a6cd5
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-23 17:27:29 +0530

    Add files via upload

---
+++ Day - 19 [23072024]/scope of varibale.py
@@ -0,0 +1,37 @@
+#Global Variable
+'''
+x = 10
+def fun():
+    print(x)
+print(x)
+fun()
+'''
+#Local Variable
+'''
+def fun():
+    x = 10
+    print(x)
+fun()
+print(x)
+'''
+#Local and Global
+'''
+x = 10
+def fun():
+    y = 20
+    print(x)
+    print(y)
+fun()
+print(x)
+print(y)
+'''
+#Local and global
+'''
+x = 10
+def fun():
+    x = 20
+    print(x)
+print(x)
+fun()
+'''
+
