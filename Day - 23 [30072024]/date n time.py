25b5687081c998c2046aa85bfc7aa6e5bf37f160
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-30 16:32:08 +0530

    Add files via upload

---
+++ Day - 23 [30072024]/date n time.py
@@ -0,0 +1,13 @@
+'''
+import datetime
+print(datetime.datetime.now())
+print(datetime.date.today())
+'''
+
+import datetime
+x = datetime.date(2024,7,29)
+print(x)
+
+from datetime import date
+y = date(2024,7,29)
+print(y)
